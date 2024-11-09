80f6441f178cc8afa0a203120e3a059350960e90
Alya Gita <alyagitaramadhani@gmail.com>
2024-11-09 17:31:30 +0700

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Search from './pages/Search'
 import Store from './pages/Store'
 import Chat from './pages/Chat'
 import Cart from './pages/Cart'
 import Checkout from './pages/Checkout'
+import Layout from './components/Layout';
 
 function App() {
 
   return (
     <>
-      <Routes>
-         <Route path='/' element={<Home/>} />
-         <Route path='/search' element={<Search/>} />
-         <Route path='/store' element={<Store/>} />
-         <Route path='/chat' element={<Chat/>} />
-         <Route path='/cart' element={<Cart/>} />
-         <Route path='/checkout' element={<Checkout/>} />
-       </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Route>
+        </Routes>
+    </Router>
     </>
   )
 }
